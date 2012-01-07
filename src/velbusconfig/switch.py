1a26968aece1086505113b9817dd97df89ef7068
Thomas Delaet <thomas@delaet.org>
2012-01-07 20:42:39 +0100

    logging cleaning

--- src/velbusconfig/switch.py
+++ src/velbusconfig/switch.py
@@ -44,7 +44,5 @@ class Switch(velbusconfig.VelbusModule):
 		"""
 		@return: None
 		"""
-		#print "Toggle Relays from switch " + binascii.hexlify(chr(self.address)) + " " + str(self.channel)
 		for relay in self.relays:
-			#print "  " + relay.to_string()
 			relay.toggle()
