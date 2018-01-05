0f84e09891078e98ffaf4af5ccfc687c21028f57
Thomas Delaet <thomas@delaet.org>
2018-01-05 23:33:19 +0100

    remove trailing characters from name

--- velbus/module.py
+++ velbus/module.py
@@ -65,7 +65,7 @@ class Module(object):
         for channel in range(1, self.number_of_channels() + 1):
             name_parts = self._name_count_data[channel]
             name = name_parts[1] + name_parts[2] + name_parts[3]
-            self._channel_names[channel] = name
+            self._channel_names[channel] = name.rstrip('\xff')
         self._name_callback(self)
         self._name_callback = None
         self._name_count_received = 0
