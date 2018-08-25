d938bc5371ba061af8401b55db886136540c5684
Maikel Punie <maikel.punie@gmail.com>
2018-08-25 09:02:25 +0200

    Add support for temperature sensors, added vmb1ts module

---
+++ velbus/modules/vmb1ts.py
@@ -0,0 +1,52 @@
+"""
+:author: Maikel Punie <maikel.punie@gmail.com>
+"""
+import velbus
+
+class VMB1TSModule(velbus.Module):
+    """
+    Velbus input module with 6 channels
+    """
+    def __init__(self, module_type, module_name, module_address, controller):
+        velbus.Module.__init__(self, module_type, module_name, module_address, controller)
+        self._cur = None
+        self._min = None
+        self._max = None
+        self._callbacks = []
+
+    def getMinTemp(self):
+        return self._min
+
+    def getMiaxTemp(self):
+        return self._max
+
+    def getCurTemp(self):
+        return self._cur
+
+    def _load(self):
+        message = velbus.ModuleStatusRequestMessage(self._address)
+        message.channels = list(range(1, self.number_of_channels()+1))
+        self._controller.send(message)
+
+    def _on_message(self, message):
+        if isinstance(message, velbus.SensorTemperatureMessage):
+            self._cur = message.cur 
+            self._min = message.min
+            self._max = message.max
+            for callback in self._callbacks:
+                callback(message.getCurTemp())
+    
+    def number_of_channels(self):
+        return 1
+
+    def on_status_update(self, channel, callback):
+        """
+        Callback to execute on status of update of channel
+        """
+        self._callbacks.append(callback)
+
+    def get_categories(self, channel):
+        return ['temp_sensor']
+
+
+velbus.register_module('VMB1TS', VMB1TSModule)
