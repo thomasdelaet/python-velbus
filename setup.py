54d3babc664c767b1d58faf50415fccea41a3b04
Thomas Delaet <thomas@delaet.org>
2019-05-31 00:20:20 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ with open('README.md') as file:
     long_description = file.read()
 
 setup(name='python-velbus',
-      version='2.0.25',
+      version='2.0.26',
       url='https://github.com/thomasdelaet/python-velbus',
       license='MIT',
       author='Thomas Delaet',
       install_requires=["pyserial==3.3"],
       author_email='thomas@delaet.org',
       description="Python Library for the Velbus protocol",
-      long_description=long_description,
       packages=['velbus', 'velbus.connections', 'velbus.messages', 'velbus.modules'],
       platforms='any',
 )
