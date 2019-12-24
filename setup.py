0b49d1757c685350d37b767a5548e9950a77415f
cereal2nd <maikel.punie@gmail.com>
2019-12-24 13:38:07 +0100

    Refactor setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 
-with open('README.md') as file:
-    long_description = file.read()
-
-setup(name='python-velbus',
-      version='2.0.31',
-      url='https://github.com/thomasdelaet/python-velbus',
-      license='MIT',
-      author='Thomas Delaet',
-      install_requires=["pyserial==3.3"],
-      author_email='thomas@delaet.org',
-      description="Python Library for the Velbus protocol",
-      packages=['velbus', 'velbus.connections', 'velbus.messages', 'velbus.modules'],
-      platforms='any',
+setup(
+    name='python-velbus',
+    version='2.0.31',
+    url='https://github.com/thomasdelaet/python-velbus',
+    license='MIT',
+    author='Thomas Delaet',
+    install_requires=["pyserial==3.3"],
+    author_email='thomas@delaet.org',
+    packages=['velbus', 'velbus.connections', 'velbus.messages', 'velbus.modules'],
 )
