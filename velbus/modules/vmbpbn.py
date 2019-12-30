f748e0e83dd074478589294779aaf3e984f94104
brefra <frank_van_breugel@hotmail.com>
2019-12-30 17:02:20 +0100

    Add LED button control

--- velbus/modules/vmbpbn.py
+++ velbus/modules/vmbpbn.py
@@ -5,6 +5,11 @@ from velbus.module import Module
 from velbus.module_registry import register_module
 from velbus.messages.push_button_status import PushButtonStatusMessage
 from velbus.messages.module_status import ModuleStatusMessage2
+from velbus.messages.update_led_status import UpdateLedStatusMessage
+from velbus.messages.set_led import SetLedMessage
+from velbus.messages.slow_blinking_led import SlowBlinkingLedMessage
+from velbus.messages.fast_blinking_led import FastBlinkingLedMessage
+from velbus.messages.clear_led import ClearLedMessage
 
 
 class VMB2PBNModule(Module):
@@ -16,6 +21,7 @@ class VMB2PBNModule(Module):
         self._is_closed = {}
         self._is_enabled = {}
         self._callbacks = {}
+        self._led_state = {}
 
     def is_closed(self, channel):
         if channel in self._is_closed:
@@ -45,6 +51,87 @@ class VMB2PBNModule(Module):
                     self._is_enabled[channel] = True
                 else:
                     self._is_enabled[channel] = False
+        elif isinstance(message, UpdateLedStatusMessage):
+            for channel in list(range(1, self.number_of_channels()+ 1)):
+                self._led_state[channel] = "off"
+                if channel in message.led_slow_blinking:
+                    self._led_state[channel] = "slow"
+                if channel in message.led_fast_blinking:
+                    self._led_state[channel] = "fast"
+                if channel in message.led_on:
+                    self._led_state[channel] = "on"
+                if channel in self._led_state and channel in self._callbacks:
+                    for callback in self._callbacks[channel]:
+                        callback(self._led_state[channel])
+        elif isinstance(message, SetLedMessage):
+            for channel in list(range(1, self.number_of_channels()+ 1)):
+                if channel in message.leds:
+                    self._led_state[channel] = "on"
+                if channel in self._led_state and channel in self._callbacks:
+                    for callback in self._callbacks[channel]:
+                        callback(self._led_state[channel])
+        elif isinstance(message, ClearLedMessage):
+            for channel in list(range(1, self.number_of_channels()+ 1)):
+                if channel in message.leds:
+                    self._led_state[channel] = "off"
+                if channel in self._led_state and channel in self._callbacks:
+                    for callback in self._callbacks[channel]:
+                        callback(self._led_state[channel])
+        elif isinstance(message, SlowBlinkingLedMessage):
+            for channel in list(range(1, self.number_of_channels()+ 1)):
+                if channel in message.leds:
+                    self._led_state[channel] = "slow"
+                if channel in self._led_state and channel in self._callbacks:
+                    for callback in self._callbacks[channel]:
+                        callback(self._led_state[channel])
+        elif isinstance(message, FastBlinkingLedMessage):
+            for channel in list(range(1, self.number_of_channels()+ 1)):
+                if channel in message.leds:
+                    self._led_state[channel] = "fast"
+                if channel in self._led_state and channel in self._callbacks:
+                    for callback in self._callbacks[channel]:
+                        callback(self._led_state[channel])
+
+    def is_on(self, channel):
+        """
+        Check if led of channel is turned on
+
+        :return: bool
+        """
+        if channel in self._led_state:
+            if self._led_state[channel] == "off":
+                return False
+            else:
+                return True
+        return False
+
+    def set_led_state(self, channel, state, callback=None):
+        """
+        Set led
+
+        :return: None
+        """
+        if callback is None:
+            def callb():
+                """No-op"""
+                pass
+            callback = callb
+        if state == "on":
+            message = SetLedMessage(self._address)
+        elif state == "slow":
+            message = SlowBlinkingLedMessage(self._address)
+        elif state == "fast":
+            message = FastBlinkingLedMessage(self._address)
+        elif state == "off":
+            message = ClearLedMessage(self._address)
+        else:
+            return
+        message.leds = [channel]
+        self._led_state[channel] = state
+        self._controller.send(message, callback)
+
+    def light_is_buttonled(self, channel):
+        return True
 
     def on_status_update(self, channel, callback):
         """
@@ -56,7 +143,7 @@ class VMB2PBNModule(Module):
 
     def get_categories(self, channel):
         if channel in self._is_enabled and self._is_enabled[channel]:
-            return ['binary_sensor']
+            return ['binary_sensor', 'light']]
         else:
             return []
 
