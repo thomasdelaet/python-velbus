8816c600644b4f20e96678b0af0ddc38ba77a9ad
Maikel Punie <maikel.punie@gmail.com>
2020-01-27 15:22:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-velbus',
-    version='2.0.36',
+    version='2.0.37',
     url='https://github.com/thomasdelaet/python-velbus',
     license='MIT',
     author='Thomas Delaet',
