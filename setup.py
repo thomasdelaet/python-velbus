7eff5b03a6819816a62c10126b45b340d49b813c
cereal2nd <maikel.punie@gmail.com>
2020-03-08 13:06:35 +0100

    Fixed the copy of the missing data file

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name="python-velbus",
-    version="2.0.41",
+    version="2.0.42",
     url="https://github.com/thomasdelaet/python-velbus",
     license="MIT",
     author="Thomas Delaet",
     install_requires=["pyserial==3.3"],
     author_email="thomas@delaet.org",
     packages=["velbus", "velbus.connections", "velbus.messages", "velbus.modules"],
+    include_package_data=True,
 )
