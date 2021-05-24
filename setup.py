c4f4b4c39e742b423e9122d05ffe17a72db3c1be
Breugel <frank_van_breugel@hotmail.com>
2021-05-24 21:43:06 +0200

    Bump version to 2.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="python-velbus",
-    version="2.1.3",
+    version="2.1.4",
     url="https://github.com/thomasdelaet/python-velbus",
     license="MIT",
     author="Thomas Delaet",
